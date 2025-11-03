f73261c3a0d93d9fae0feeca3524c3c6ab70acb1
Dominic Goodfellow <dominicgoodfellow98@gmail.com>
2025-11-03 16:33:04 -0500

    fix: make sidebar, header, footer sticky - only chat scrolls

--- frontend/src/components/layout/AppLayout.tsx
+++ frontend/src/components/layout/AppLayout.tsx
@@ -1,3 +1,4 @@
+// src/components/layout/AppLayout.tsx
 import { Outlet } from 'react-router-dom'
 import { Sidebar } from '@/components/sidebar/Sidebar'
 import { Header } from '@/components/header/Header'
@@ -6,18 +7,21 @@ import { ToastContainer } from '@/components/ui/ToastContainer'
 
 export function AppLayout() {
   return (
-    <div className="min-h-screen flex">
-      {/* Sidebar */}
+    <div className="h-screen flex overflow-hidden">
+      {/* Sidebar - Sticky on left */}
       <Sidebar />
       
-      {/* Main content area */}
-      <div className="flex-1 flex flex-col min-w-0">
+      {/* Main content area - Flex column with header, content, footer */}
+      <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
+        {/* Header - Sticky at top */}
         <Header />
         
-        <main className="flex-1 flex flex-col overflow-hidden">
+        {/* Main scrollable area - ONLY THIS SCROLLS */}
+        <main className="flex-1 overflow-y-auto overflow-x-hidden">
           <Outlet />
         </main>
         
+        {/* Footer - Sticky at bottom */}
         <Footer />
       </div>
       
